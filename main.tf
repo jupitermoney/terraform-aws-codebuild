b422541778f7139d36445d661d5f30fcc1af41ee
Sanjeev Pandey <sanjeev.pandey@jupiter.money>
2020-03-25 02:37:40 +0530

    Removed IAM Role

--- main.tf
+++ main.tf
@@ -78,106 +78,11 @@ locals {
   cache = local.cache_options[var.cache_type]
 }
 
-resource "aws_iam_role" "default" {
-  count              = var.enabled ? 1 : 0
-  name               = module.label.id
-  assume_role_policy = data.aws_iam_policy_document.role.json
-}
-
-data "aws_iam_policy_document" "role" {
-  statement {
-    sid = ""
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-    }
-
-    effect = "Allow"
-  }
-}
-
-resource "aws_iam_policy" "default" {
-  count  = var.enabled ? 1 : 0
-  name   = module.label.id
-  path   = "/service-role/"
-  policy = data.aws_iam_policy_document.permissions.json
-}
-
-resource "aws_iam_policy" "default_cache_bucket" {
-  count  = var.enabled && local.s3_cache_enabled ? 1 : 0
-  name   = "${module.label.id}-cache-bucket"
-  path   = "/service-role/"
-  policy = join("", data.aws_iam_policy_document.permissions_cache_bucket.*.json)
-}
-
-data "aws_iam_policy_document" "permissions" {
-  statement {
-    sid = ""
-
-    actions = [
-      "codecommit:GitPull",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:GetAuthorizationToken",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-      "ecs:RunTask",
-      "iam:PassRole",
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "ssm:GetParameters",
-    ]
-
-    effect = "Allow"
-
-    resources = [
-      "*",
-    ]
-  }
-}
-
-data "aws_iam_policy_document" "permissions_cache_bucket" {
-  count = var.enabled && local.s3_cache_enabled ? 1 : 0
-
-  statement {
-    sid = ""
-
-    actions = [
-      "s3:*",
-    ]
-
-    effect = "Allow"
-
-    resources = [
-      join("", aws_s3_bucket.cache_bucket.*.arn),
-      "${join("", aws_s3_bucket.cache_bucket.*.arn)}/*",
-    ]
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "default" {
-  count      = var.enabled ? 1 : 0
-  policy_arn = join("", aws_iam_policy.default.*.arn)
-  role       = join("", aws_iam_role.default.*.id)
-}
-
-resource "aws_iam_role_policy_attachment" "default_cache_bucket" {
-  count      = var.enabled && local.s3_cache_enabled ? 1 : 0
-  policy_arn = join("", aws_iam_policy.default_cache_bucket.*.arn)
-  role       = join("", aws_iam_role.default.*.id)
-}
 
 resource "aws_codebuild_project" "default" {
   count         = var.enabled ? 1 : 0
   name          = module.label.id
-  service_role  = join("", aws_iam_role.default.*.arn)
+  service_role  = var.codebuild_iamrole
   badge_enabled = var.badge_enabled
   build_timeout = var.build_timeout
 
@@ -197,30 +102,6 @@ resource "aws_codebuild_project" "default" {
     type            = "LINUX_CONTAINER"
     privileged_mode = var.privileged_mode
 
-    environment_variable {
-      name  = "AWS_REGION"
-      value = signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name
-    }
-    environment_variable {
-      name  = "AWS_ACCOUNT_ID"
-      value = signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id
-    }
-    environment_variable {
-      name  = "IMAGE_REPO_NAME"
-      value = signum(length(var.image_repo_name)) == 1 ? var.image_repo_name : "UNSET"
-    }
-    environment_variable {
-      name  = "IMAGE_TAG"
-      value = signum(length(var.image_tag)) == 1 ? var.image_tag : "latest"
-    }
-    environment_variable {
-      name  = "STAGE"
-      value = signum(length(var.stage)) == 1 ? var.stage : "UNSET"
-    }
-    environment_variable {
-      name  = "GITHUB_TOKEN"
-      value = signum(length(var.github_token)) == 1 ? var.github_token : "UNSET"
-    }
 
     dynamic "environment_variable" {
       for_each = var.environment_variables
